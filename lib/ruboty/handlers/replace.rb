c32b2caab3aafae27b21dd5c5892646bae255b9b
Ryo Nakamura <r7kamura@gmail.com>
2015-03-05 23:01:38 +0900

    Add #register and #list implementation

--- lib/ruboty/handlers/replace.rb
+++ lib/ruboty/handlers/replace.rb
@@ -1,14 +1,15 @@
 require "ruboty"
+require "yaml"
 
 module Ruboty
   module Handlers
     class Replace < Base
+      BRAIN_NAMESPACE = "ruboty-replace"
+
       on(
-        //,
-        all: true,
-        description: "Replace message with registered patterns",
-        hidden: true,
-        name: :replace,
+        /list patterns/,
+        description: "List registered patterns to replace",
+        name: :list
       )
 
       on(
@@ -17,12 +18,39 @@ module Ruboty
         name: :register,
       )
 
+      on(
+        //,
+        all: true,
+        description: "Replace message with registered patterns",
+        hidden: true,
+        name: :replace,
+      )
+
+      def list(message)
+        message.reply(patterns_in_string, code: true)
+      end
+
       def register(message)
-        puts "TODO"
+        store[message[:pattern]] = message[:template]
+        message.reply("Registered!")
       end
 
       def replace(message)
         puts "TODO"
+      end
+
+      private
+
+      def patterns_in_string
+        if store.empty?
+          "No pattern registered"
+        else
+          store.to_yaml[4..-1]
+        end
+      end
+
+      def store
+        robot.brain.data[BRAIN_NAMESPACE] ||= {}
       end
     end
   end
